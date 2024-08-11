f581f26d121c1a81ca8a289d860d4c7d7e6d479d
Rajveer malviya <malviyarajveer777@gmail.com>
2024-08-11 22:09:37 +0530

    website

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,67 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+import { SparklesPreview } from "@/components/backgroud/sparkle";
+import { SpotlightPreview } from "@/components/backgroud/spotlite";
+import  Container  from "../components/global/container";
+
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import Wrapper from "@/components/global/wrapper";
+import { features, perks } from "@/components/backgroud";
+import Footer from "@/components/footer";
+
+export default async function Page() {
+  const session = await getServerSession();
+return <section>
+  <Wrapper>
+  <div>
+        <SpotlightPreview></SpotlightPreview>
+  </div>
+  <div>
+    <Container>
+        <SparklesPreview></SparklesPreview>
+    </Container>
+  </div>
+  <div className="hidden lg:block absolute top-[30rem] -right-[16%] bg-primary-red w-72 h-72 rounded-full z-10 blur-[14rem]"></div>
+  <div className="hidden lg:block absolute bottom-0 -left-1/4 bg-primary-blue w-96 h-72 rounded-full z-100 blur-[14rem]"></div>
+  <div className="hidden lg:block absolute -bottom-[40rem] -left-1/4 bg-primary-blue w-96 h-40 rounded-full z-100 blur-[14rem]"></div>
+  <Container>
+
+    <div className="mt-20 justify-center font-semibold text-center w-full lg:text-4xl bg-gradient-to-b  from-neutral-50 to-neutral-400 inline-block text-transparent bg-clip-text">
+      Where <a className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">Innovation</a>  Takes Shape: Crafting Robots
+    </div>
+
+  <div className="mt-5 p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-500">
+        Everything you need to <a className="bg-gradient-to-r from-red-500 via-purple-500 to-indigo-500 text-transparent bg-clip-text">build</a> your next robot
+        </h1>
+        <p className="mt-4 pt-2 md:text-2xl font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+        Talk with professionals, researchers, and hobbyists.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
+      <Container>
+                    <div className="flex flex-col items-center justify-center py-10 md:py-20 w-full">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full divide-x-0 md:divide-x divide-y md:divide-y-0 divide-gray-900 first:border-l-2 lg:first:border-none first:border-gray-900">
+                            {perks.map((perk) => (
+                                <div key={perk.title} className="flex flex-col items-start px-4 md:px-6 lg:px-8 lg:py-6 py-4">
+                                    <div className="flex items-center justify-center">
+                                        <perk.icon className="w-8 h-8" />
+                                    </div>
+                                    <h3 className="text-lg font-medium mt-4">
+                                        {perk.title}
+                                    </h3>
+                                    <p className="text-muted-foreground mt-2 text-start lg:text-start">
+                                        {perk.info}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </Container>
+      <div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      </Container>
+      <Footer></Footer>
+  </Wrapper>
+</section>
 }
---
+++ components/backgroud/index.ts
@@ -0,0 +1,85 @@
+import Icons from "../global/icons";
+
+export const perks = [
+    {
+        icon: Icons.bolt,
+        title: "buy and sell",
+        info: "Buy and sell completed robots, parts, and designs.",
+    },
+    {
+        icon: Icons.customize,
+        title: "find parts",
+        info: "Actuators, motors, and more! Buy, sell, and read about any part. Get recommendations and suggestions to help you with your project.",
+    },
+    {
+        icon: Icons.launch,
+        title: "join the community",
+        info: "Join the K-Scale discord for Q&A and to see what other people are building.",
+    },
+];
+
+export const features = [
+    {
+        icon: Icons.bolt,
+        title: "Fast Setup",
+        info: "Get your website up and running in minutes with our intuitive AI-powered builder.",
+    },
+    {
+        icon: Icons.palette,
+        title: "Customizable Templates",
+        info: "Choose from a variety of stunning templates and customize them to suit your brand.",
+    },
+    {
+        icon: Icons.seo,
+        title: "SEO Optimized",
+        info: "Built-in SEO features ensure your website ranks well on search engines.",
+    },
+    {
+        icon: Icons.monitor,
+        title: "Responsive Design",
+        info: "Your website will look great on any device, from desktops to mobile phones.",
+    },
+    {
+        icon: Icons.shop,
+        title: "E-Commerce Ready",
+        info: "Start selling online with our e-commerce features and integrations.",
+    },
+    {
+        icon: Icons.server,
+        title: "Secure Hosting",
+        info: "Enjoy peace of mind with secure and reliable hosting for your website.",
+    },
+];
+
+export const pricingCards = [
+    {
+        title: "Starter",
+        description: "Perfect for trying out plura",
+        price: "Free",
+        duration: "",
+        highlight: "Key features",
+        buttonText: "Start for free",
+        features: ["Limited projects", "1 Team member", "Basic features"],
+        priceId: "",
+    },
+    {
+        title: "Unlimited Saas",
+        description: "The ultimate agency kit",
+        price: "$199",
+        duration: "month",
+        highlight: "Key features",
+        buttonText: "Upgrade to Pro",
+        features: ["Unlimited projects", "5 Team members", "Advanced design tools", "Customizable domain"],
+        priceId: "price_1OYxkqFj9oKEERu1KfJGWxgN",
+    },
+    {
+        title: "Enterprise",
+        description: "For serious agency owners",
+        price: "$399",
+        duration: "month",
+        highlight: "Everything in Starter, plus",
+        buttonText: "Upgrade to Enterprise",
+        features: ["Unlimited projects", "Unlimited Team members", "Custom branding", "Priority support (24/7)"],
+        priceId: "price_1OYxkqFj9oKEERu1NbKUxXxN",
+    },
+];
