5316565e2a73863c2a2b4d67783da8ef79dd13a9
Rajveer malviya <malviyarajveer777@gmail.com>
2024-08-15 13:15:51 +0530

    crated-signup-page

--- app/(dashboard)/signin/page.tsx
+++ app/(dashboard)/signin/page.tsx
@@ -1,3 +1,32 @@
-export default function Signin(){
-    return <></>
+
+import { BottomWarning } from "@/components/objects/bottomWarning";
+import { Signin } from "@/components/objects/signin";
+import { Signup } from "@/components/objects/signup";
+
+
+
+export default function signup() {
+    
+     return <div className="h-screen flex justify-center flex-col pt-14 pb-10">
+         <div className="hidden lg:block absolute -top-[5rem] -right-[10%] bg-primary-pink w-72 h-72 rounded-full z-10 blur-[14rem]"></div>
+         <div className="hidden lg:block absolute -bottom-20 -left-1/4 bg-primary-blue w-96 h-72 rounded-full z-100 blur-[14rem]"></div>
+        
+        <div className="flex justify-center ">
+        <a href="#" className="block max-w-[30rem] p-8  border border-slate-600 rounded-lg shadow  ">
+                <div>
+                    <div className="px-10  ">
+                        <div className="text-3xl ml-5 font-extrabold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-700 text-transparent bg-clip-text">   
+                            Sign In
+                        </div>
+                    </div>
+                    <div className="pt-2">                      
+                        <Signin></Signin>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div className="z-10 -mt-10 ">
+         <BottomWarning label={"dont have an account?"} href={"/signup"} buttonText={"Sign Up"}></BottomWarning> 
+        </div>
+    </div>
 }
---
+++ components/objects/signin.tsx
@@ -0,0 +1,46 @@
+"use client"
+import { useState } from "react"
+import { LabelledInput } from "./textinput"
+import { FcGoogle } from "react-icons/fc"
+import { signIn } from "next-auth/react"
+import { CreateUser } from "@/lib/actions/CreateUser"
+import { useRouter } from "next/navigation"
+
+export function Signin(){
+    const router = useRouter()
+    const  [email , setEmail ] = useState("")
+    const  [password , setPassword ] = useState("")
+
+return <div>
+            <div>              
+                <LabelledInput label="email" placeholder="123@gmail.com" onChange={(value)=>{
+                    setEmail(value)
+                }} />
+                <LabelledInput label="password" placeholder="ad13123@" onChange={(value)=>{
+                    setPassword(value)
+                }} />
+            </div>
+    <div className="my-4 pt-1 ml-14 ">
+                            <button onClick={async()=>{
+                            const res =  await signIn('credentials',{
+                                email : email,
+                                password : password,
+                                redirect : false
+                             })
+                             router.push("/")
+                            }} className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 w-32">
+                                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                                    signup
+                                </span>
+                            </button>
+                            <button onClick={async()=>{
+                                await signIn("google")
+                            }} className=" mt-5 -ml-7 flex items-center p-2 text-black bg-gray-200 rounded">
+                        <FcGoogle size={24} className="mr-2" />
+                        Sign in with Google
+                        </button>
+                        </div>
+          
+        </div>
+}
