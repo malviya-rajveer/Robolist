2be63cc03d69bbac930407679092fa581b461b92
Rajveer malviya <malviyarajveer777@gmail.com>
2024-08-22 16:27:29 +0530

    navbar correction for small screen

--- components/objects/Appbar.tsx
+++ components/objects/Appbar.tsx
@@ -3,7 +3,7 @@ import { signOut } from "next-auth/react";
 import { Button } from "./button";
 
 export function AppBar(){
-    return <div className="flex items-center gap-4 pl-2 lg:pl-48 justify-end ">
+    return <div className="flex items-center gap-4 pl- lg:pl-48 justify-end ">
             <Button onclick={()=>{}} href ={"/signup"}>signup</Button>
             <Button onclick={async()=>{await signOut({redirect : false})}} href ={"/"}>signout</Button>
         </div>
